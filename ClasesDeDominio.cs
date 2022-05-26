42f82de665e01fd8fee16987b5b57dc94475370f
sxxor <xreyes596@gmail.com>
2022-05-26 16:32:32 -0500

    Add files via upload

---
+++ ClasesDeDominio.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BDD_ProyectoFinal {
+
+  public class ClasesDeDominio {
+
+    public class Cuenta {
+      public Decimal Saldo { get; set; }
+
+      public decimal RetirarDinero(decimal monto) {
+        if (monto > Saldo)
+          return 0;
+        else {
+          Saldo -= monto;
+          return monto;
+        }
+      }
+    }
+
+    public class Tarjeta {
+      public Estadotarjeta estadoTarjeta { get; set; }
+    }
+
+    public class CajeroAutomatico {
+      public Tarjeta tarjeta { get; set; }
+      public decimal AvailableCash { get; set; }
+      public IList<Tarjeta> Retenertarjeta { get; private set; }
+      public string Mensaje { get; private set; }
+
+      public CajeroAutomatico() {
+        Retenertarjeta = new List<Tarjeta>();
+      }
+
+      public Decimal ProcesarSolicitud(Cuenta cuenta, decimal monto) {
+        decimal efectivo = cuenta.RetirarDinero(monto);
+
+        if (efectivo == 0) {
+          Mensaje = "Hay fondos insuficientes disponibles en su cuenta";
+        } else {
+          Mensaje = "Por favor toma su efectivo";
+        }
+
+        tarjeta = null;
+        return efectivo;
+      }
+    }
+
+    public enum Estadotarjeta {
+      Valida,
+      Invalida
+    }
+  }
+}
---
+++ Titular_Cuenta_retira_efectivo.cs
@@ -0,0 +1,47 @@
+﻿using Be.Corebvba.Aubergine;
+
+namespace BDD_ProyectoFinal {
+  public class Titular_Cuenta_retira_efectivo : Story<BDD_Cuenta> {
+
+    private As_an Titular_Cuenta;
+    private I_want retirar_efectivo_de_un_cajero_automatico;
+    private So_that Poder_obtener_dinero_cuando_el_banco_este_cerrado;
+
+    private class La_Cuenta_tiene_fondos_suficientes_Para_Retiros : Scenario {
+      private Given el_saldo_de_la_cuenta_es_100;
+      private Given la_tarjeta_es_Valida;
+      private Given el_cajeroautomatico_contiene_suficiente_dinero;
+
+      private When el_titular_de_la_cuenta_solicita_20;
+
+      private Then el_cajero_automatico_debe_entregar_20;
+      private Then el_saldo_de_la_cuenta_debe_ser_80;
+      private Then entregar_tarjeta;
+    }
+
+    private class Fondos_insuficientes_En_La_Cuenta : Scenario {
+      private Given el_saldo_de_la_cuenta_es_10;
+      private Given la_tarjeta_es_Valida;
+      private Given el_cajeroautomatico_contiene_suficiente_dinero;
+
+      private When el_titular_de_la_cuenta_solicita_20;
+
+      private Then el_cajero_automatico_no_debe_entregar_ningun_dinero;
+      private Then el_cajero_automatico_debe_decir_que_hay_fondos_insuficientes;
+
+      private Then el_saldo_de_la_cuenta_debe_ser_10;
+      private Then entregar_tarjeta;
+    }
+
+    private class La_Tarjeta_Ha_Sido_deshabilitada : Scenario {
+      private Given la_tarjeta_es_Invalida;
+
+      private When the_Account_Holder_requests_20;
+
+      private Then el_cajero_automatico_debe_retener_la_tarjeta;
+
+      private Then the_ATM_should_say_the_card_has_been_retained;
+    }
+
+  }
+}
